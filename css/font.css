75486fc170f9719b287d14249a4d9c4ded75d590
Abhinav Rajagopalan <abhinavrajagopalan@gmail.com>
2018-06-07 00:38:01 +0000

    Add new file

---
+++ css/font.css
@@ -0,0 +1,26 @@
+<style>
+  @font-face {
+    font-family: "Computer Modern";
+    src: url('http://spratt.github.io/Computer-Modern/cmunss.otf');
+  }
+  @font-face {
+    font-family: "Computer Modern";
+    src: url('http://spratt.github.io/Computer-Modern/cmunsx.otf');
+    font-weight: bold;
+  }
+  @font-face {
+    font-family: "Computer Modern";
+    src: url('http://spratt.github.io/Computer-Modern/cmunsi.otf');
+    font-style: italic, oblique;
+  }
+  @font-face {
+    font-family: "Computer Modern";
+    src: url('http://spratt.github.io/Computer-Modern/cmunbxo.otf');
+    font-weight: bold;
+    font-style: italic, oblique;
+  }
+
+  body {
+    font-family: "Computer Modern", serif;
+  }
+</style>
